ecd1e2bf98d7f2dbc84c91e8e1339b794614ec88
mffabrigas <migfabrigas@gmail.com>
2020-05-29 21:37:53 -0500

    Add missing semi-colon

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-let cursor = true
+let cursor = true;
 const speed = 250;
 
 setInterval(() => {
