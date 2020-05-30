9a63da71b8ccf6766cbcad9eec03d4c8ba84010f
mffabrigas <migfabrigas@gmail.com>
2020-05-29 19:16:29 -0500

    create minimum viable product

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,17 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+let cursor = true
+const speed = 250;
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+setInterval(() => {
+  if(cursor) {
+    document.getElementById('cursor').style.opacity = 0;
+    cursor = false;
+  } else {
+    document.getElementById('cursor').style.opacity = 1;
+    cursor = true;
+  }
+}, speed);
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
