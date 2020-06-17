9a72b98d6b1e96a64f081a1acf3308963972d0ae
mffabrigas <migfabrigas@gmail.com>
2020-06-17 19:35:45 +0000

    Rename HomeServlet to AuthenticationServlet

--- portfolio/src/main/java/com/google/sps/servlets/AuthenticationServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/AuthenticationServlet.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/login")
-public class HomeServlet extends HttpServlet {
+public class AuthenticationServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
